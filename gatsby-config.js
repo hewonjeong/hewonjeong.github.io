7b142bc5503a7cb6b8a4e0a32f06cbf93e969c39
Hewon Jeong <hewon.jng@gmail.com>
2019-06-18 03:10:22 +0900

    feat: gatsby-config 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Hewon Jeong`,
+    author: `Hewon Jeong`,
+    description: `Personal blog by Hewon Jeong`,
+    siteUrl: `https://hewonjeong.github.io`,
     social: {
       twitter: `kylemathews`,
     },
