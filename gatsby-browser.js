5d0faeec12705482f2a11da21a718440204bfe03
Hewon Jeong <hewon.jng@gmail.com>
2019-06-25 20:09:40 +0900

    design: add font (noto-sans-kr)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
 // custom typefaces
+import "typeface-noto-sans-kr"
 import "typeface-montserrat"
 import "typeface-merriweather"
--- src/components/layout.js
+++ src/components/layout.js
@@ -13,8 +13,8 @@ class Layout extends React.Component {
       header = (
         <h1
           style={{
-            ...scale(1.1),
-            marginBottom: rhythm(1.5),
+            ...scale(1),
+            marginBottom: rhythm(1.2),
             marginTop: 0,
           }}
         >
