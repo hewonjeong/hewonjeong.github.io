d95c8c84312ed40488c7d4dab9d9c00d8313783d
Hewon Jeong <hewon.jng@gmail.com>
2023-04-17 23:30:22 +0900

    feat: update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,71 +1,26 @@
-import React from 'react'
+import * as React from 'react'
 import { Link } from 'gatsby'
 
-import { rhythm, scale } from '../utils/typography'
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
 
-class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
-
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(0.75),
-            fontFamily: 'Montserrat, sans-serif',
-            marginBottom: rhythm(1.2),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
+  return (
+    <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <header className="global-header">
+        {isRootPath ? (
+          <h1 className="main-heading">
+            <Link to="/">{title}</Link>
+          </h1>
+        ) : (
+          <Link className="header-link-home" to="/">
             {title}
           </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
-    return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-      </div>
-    )
-  }
+        )}
+      </header>
+      <main>{children}</main>
+    </div>
+  )
 }
 
 export default Layout
