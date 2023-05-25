daffa79f945a7b0726f091c3f739d597f349494b
Hewon Jeong <hewon.jng@gmail.com>
2023-05-25 19:10:29 +0900

    perf: fonts improvement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,4 @@
-// custom typefaces
-// import "typeface-noto-sans-kr"
-// import "typeface-montserrat"
-
-import '@fontsource/montserrat/900.css'
+import './src/styles/Montserrat.css'
 
 import './src/styles/global.css'
 // import './src/styles/code.css'
