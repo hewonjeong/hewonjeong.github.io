13e8f18ad2b08b142b28ea3ac5c3baf61185d675
Hewon Jeong <hewon.jng@gmail.com>
2023-07-30 12:33:13 +0900

    feat: Twitter’s new branding

--- src/components/layout.js
+++ src/components/layout.js
@@ -58,9 +58,9 @@ function Social() {
           link: 'https://github.com/hewonjeong',
         },
         {
-          label: 'Twitter',
-          icon: <Icon.Twitter />,
-          link: 'https://twitter.com/hewonjeong',
+          label: 'X formerly known as Twitter',
+          icon: <Icon.X />,
+          link: 'https://x.com/hewonjeong',
         },
       ].map(({ label, icon, link }) => (
         <a
@@ -104,17 +104,18 @@ const Icon = {
       </svg>
     )
   },
-  Twitter() {
+  X() {
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
         width="1.5em"
         height="1.5em"
+        viewBox="-2 -2 28 28"
         fill="currentColor"
       >
-        <path fill="none" d="M0 0h24v24H0z" />
-        <path d="M22.162 5.656a8.384 8.384 0 0 1-2.402.658A4.196 4.196 0 0 0 21.6 4c-.82.488-1.719.83-2.656 1.015a4.182 4.182 0 0 0-7.126 3.814 11.874 11.874 0 0 1-8.62-4.37 4.168 4.168 0 0 0-.566 2.103c0 1.45.738 2.731 1.86 3.481a4.168 4.168 0 0 1-1.894-.523v.052a4.185 4.185 0 0 0 3.355 4.101 4.21 4.21 0 0 1-1.89.072A4.185 4.185 0 0 0 7.97 16.65a8.394 8.394 0 0 1-6.191 1.732 11.83 11.83 0 0 0 6.41 1.88c7.693 0 11.9-6.373 11.9-11.9 0-.18-.005-.362-.013-.54a8.496 8.496 0 0 0 2.087-2.165z" />
+        <g>
+          <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+        </g>
       </svg>
     )
   },
