9cfc7d3f224bdbcfde19137c16180415a6a62088
Hewon Jeong <hewon.jng@gmail.com>
2024-03-05 19:22:11 +0100

    footer

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -1,15 +1,5 @@
 type Props = { children: React.ReactNode }
 
 export default function Layout({ children }: Props) {
-  return (
-    <>
-      {children}
-      <footer className="flex items-center justify-between">
-        <span className="text-neutral-500">
-          Hewon Jeong © {new Date().getFullYear()}
-        </span>
-        <div>Socials Here!</div>
-      </footer>
-    </>
-  )
+  return <>{children}</>
 }
