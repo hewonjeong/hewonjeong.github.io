f56c3d563ec2e5100d7c436971cd1f54f4908f40
Hewon Jeong <hewon.jng@gmail.com>
2024-03-12 22:59:56 +0900

    single css

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -1,5 +1,3 @@
-import './markdown.css'
-
 type Props = { children: React.ReactNode }
 
 export default function Layout({ children }: Props) {
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,7 @@
-import './[slug]/markdown.css'
-
 export default function NotFound() {
   return (
     <article className="markdown">
-      <h1 className="text-[40px] font-bold leading-[44px]']">
-        Not found
-      </h1>
+      <h1 className="text-[40px] font-bold leading-[44px]']">Not found</h1>
       <div className="markdown mt-10">
         <p>This page doesn’t exist (yet?)</p>
       </div>
